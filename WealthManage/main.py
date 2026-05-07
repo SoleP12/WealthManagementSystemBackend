099ea8f32144d503dbd9a568b5b71f1799b2ead5
Jon <135755963+SoleP12@users.noreply.github.com>
2026-05-07 00:42:57 +0000

    *Changing of frontend folder name to templates for jinja2

--- WealthManage/main.py
+++ WealthManage/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+# Creation of Template object
+templates = Jinja2Templates(directory = "templates")
+
+
+
+
+
+
+
 
 @app.get("/")
 async def root():
