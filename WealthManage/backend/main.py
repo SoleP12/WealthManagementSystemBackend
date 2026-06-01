d24a1e9b83fd4e9f6cbee7fdcfbbbd5dbdc83f51
Jon <135755963+SoleP12@users.noreply.github.com>
2026-06-01 20:46:31 +0000

    Database Fixing and Models Fixing. Incorporating  of hashing password after delete and update CRUD abilities.

--- WealthManage/backend/main.py
+++ WealthManage/backend/main.py
@@ -1,10 +1,10 @@
 # source venv/bin/activate
 ##############################################
 # FastApi Creation Imports, Exception Imports
-from fastapi import Request
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
 
 ##############################################
 from pathlib import Path
@@ -17,10 +17,15 @@ import uvicorn
 ##############################################
 # Model Imports
 from . import models
+from .models import WealthManager
+
+##############################################
+# Schema Import
+from .schemas import WealthManagerResponse, WealthManagerCreate, WealthManagerBase
 
 ##############################################
 # Database Imports
-from .database import Base, engine
+from .database import Base, engine, get_db
 
 ##############################################
 
@@ -44,6 +49,33 @@ templates = Jinja2Templates(directory =BASE_DIR /  "templates")
 @app.get("/", name = "Login")
 async def default_page(request: Request):
     return templates.TemplateResponse(request, "login.html")
+
+# Creation of WealthManager
+@app.post("/wealthmanager/", response_model = WealthManagerResponse)
+def create_user(wealthmanager: WealthManagerCreate, db:Session = Depends(get_db)):
+    if db.query(WealthManager).filter(WealthManager.email == wealthmanager.email).first():
+        raise HTTPException(status_code = 404, detail = "WealthManager Already Exisit")
+    # Creation of New Wealth Manager
+    wealth_manager = WealthManager(**wealthmanager.dict())
+    db.add(wealth_manager)
+    db.commit()
+    db.refresh(wealth_manager)
+    return wealth_manager
+
+# Get Specific WealthManager
+@app.get("/users/{wealthmanager_id}", response_model = WealthManagerResponse)
+def get_users(wealthmanager_id: int, db:Session = Depends(get_db)):
+    user = db.query(WealthManager).filter(WealthManager.id == wealthmanager_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail = "WealthManager not found")
+
+
+
+@app.get("/allWealthManagers/")
+def showcasedatabase():
+    db.query(WealthManager)
+
+
 
 
 
--- WealthManage/backend/models.py
+++ WealthManage/backend/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy.ext.declarative import declarative_base
+from .database import Base
 
-Base = declarative_base()
 
 class WealthManager(Base):
     __tablename__ = "items"
@@ -9,6 +8,6 @@ class WealthManager(Base):
     id = Column(Integer, primary_key = True, index= True)
     name = Column(String, index = True, nullable=False)
     email = Column(String, unique = True)
-    hased_password = Column(String, nullable = False)
+    password = Column(String, nullable = False)
     net_worth = Column(Float, default = 0.0)
     
