aef657b82f2cb6fd6cb6c94469852d5956bbd2b8
Jon <135755963+SoleP12@users.noreply.github.com>
2026-06-11 07:35:56 +0000

    fix(auth): correct credentials_exception variable name

--- WealthManage/backend/auth.py
+++ WealthManage/backend/auth.py
@@ -59,19 +59,19 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) ->st
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-async def get_current_user(token : Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db),):
+async def get_current_user(token : Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
     credentials_exeception = HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "Could not validate credentials", headers = {"WWWW-Authenticate": "Bearer"})
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
+            raise credentials_exeception
         token_data = TokenData(username = username)
     except InvalidTokenError:
-        raise credentials_exception
+        raise credentials_exeception
     user = db.query(WealthManager).filter(WealthManager.email == token_data.username).first()
     if user is None:
-        raise credentials_exception
+        raise credentials_exeception
     return user
 
 async def get_current_active_user(current_user: Annotated[WealthManager, Depends(get_current_user)]):
